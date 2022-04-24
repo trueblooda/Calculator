23051cbdfa28a381ce1d5fedb9ca2a62aed79a38
Adison Trueblood <adisontrueblood3@gmail.com>
2022-04-24 12:32:33 -0400

    Update 2 fixed user inputs made it a little smaller added notes on what I need to do changed ParseInt to ParseFloat to work with decimals

--- calculator.js
+++ calculator.js
@@ -1,20 +1,21 @@
 //Calling a prompt to get user input for the calculator
+const question1 = 'Would you like to multiply, add, divide, or subtract?'
 var prompt = require('prompt');
 prompt.start();
-prompt.get(['Value1','Value2','question'], function (err, result) {    
+prompt.get(['Value1','Value2', 'question'], function (err, result) {    
+    //TODO: ****get this to output before it runs the equation****
     console.log('Would you like to multiply, add, divide, or subtract?')
-
    //Start of If statement for the math part of the calcultor
-    if (result.question === '+') {
-        console.log(parseInt(result.Value1) + parseInt(result.Value2))
+    if (result.question === 'add') {
+        console.log(parseFloat(result.Value1) + parseFloat(result.Value2))
     }
-else if(result.question === '-'){
-    console.log(parseInt(result.Value1) - parseInt(result.Value2))
+else if(result.question === 'subtract'){
+    console.log(parseFloat(result.Value1) - parseFloat(result.Value2))
 }
-else if(result.question === '*'){
-    console.log(parseInt(result.Value1) * parseInt(result.Value2))
+else if(result.question === 'multiply'){
+    console.log(parseFloat(result.Value1) * parseFloat(result.Value2))
 }
-else if(result.question === '/'){
-    console.log(parseInt(result.Value1) / parseInt(result.Value2))
+else if(result.question === 'divide'){
+    console.log(parseFloat(result.Value1) / parseFloat(result.Value2))
 }
 });
